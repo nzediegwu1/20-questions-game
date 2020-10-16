11747e340d7c024c131d51d30e4fef8d10cf1342
Anaeze <nzediegwu1@gmail.com>
2020-10-16 21:24:06 +0100

    fix(online-users): clear online-users table on app launch

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 import config from './config';
 import { http } from './app';
+import { OnlineUsers } from './models';
 
 const port = config.PORT;
 
 http.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+  OnlineUsers.deleteMany({}).exec();
 });
